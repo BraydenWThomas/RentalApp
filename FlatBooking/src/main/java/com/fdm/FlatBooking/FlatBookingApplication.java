26aed2b012929923de85805a3a2dc9a3267fed96
Sam <samantha.a.jermyn@gmail.com>
2023-10-09 16:35:13 +1100

    Generic Java Template
    Template created

---
+++ FlatBooking/src/main/java/com/fdm/FlatBooking/FlatBookingApplication.java
@@ -0,0 +1,13 @@
+package com.fdm.FlatBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatBookingApplication.class, args);
+	}
+
+}
---
+++ FlatBooking/src/test/java/com/fdm/FlatBooking/FlatBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdm.FlatBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
