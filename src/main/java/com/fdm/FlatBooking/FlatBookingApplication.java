f97281d0433187c709bc22d36c1ac875fcaa6491
Sam <samantha.a.jermyn@gmail.com>
2023-10-10 12:13:17 +1100

    User Test
    tested Mongo connection

--- src/main/java/com/fdm/FlatBooking/FlatBookingApplication.java
+++ src/main/java/com/fdm/FlatBooking/FlatBookingApplication.java
@@ -1,13 +1,35 @@
 package com.fdm.FlatBooking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.fdm.FlatBooking.Model.User;
+import com.fdm.FlatBooking.Repository.UserRepository;
 
 @SpringBootApplication
-public class FlatBookingApplication {
+@EnableMongoRepositories
+public class FlatBookingApplication implements CommandLineRunner{
+	
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlatBookingApplication.class, args);
+	}
+
+	public void run(String... args) {
+        System.out.println("-----CREATE User ITEMS-----\n");
+        createUser();
+        System.out.println("\n-----USER CREATED-----\n");
+    }
+
+	private void createUser() {
+        userRepository.save(new User("sjer", "samantha jermyn", 20, "manager"));
+
+		
 	}
 
 }
