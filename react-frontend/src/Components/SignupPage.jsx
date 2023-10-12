fe26092ecaf18640de8dcdd85c84cacd3d646a61
Jack A <82874057+Jackile13@users.noreply.github.com>
2023-10-12 17:54:30 +1100

    continued signup page

--- react-frontend/src/Components/SignupPage.jsx
+++ react-frontend/src/Components/SignupPage.jsx
@@ -1,26 +1,43 @@
-import { Button, Grid, TextField, Stack } from "@mui/material";
+import { Button, Grid, TextField, Stack, Container } from "@mui/material";
 import "../Styles/SignupPage.css"
 
 const SignupPage = () => {
     
 
     return (
-        <Grid container>
-            <Grid xs={1}/>
-            <Grid xs={10} className="formbox">
-                <Stack direction="row" spacing={5}>
-                    <Stack direction="column">
-                        <p>First Name:</p>
-                        <TextField/>
-                    </Stack>
-                    <Stack direction="column">
-                        <p>Last Name:</p>
-                        <TextField/>
-                    </Stack>
-                </Stack>
-            </Grid>
-            <Grid xs={1}/>
-        </Grid>
+        <Container maxWidth="sm">
+            <div className="formbox">
+                    <form>
+                        <Grid container>
+                            <Grid xs={6} paddingInline={2}>
+                                <p className="fieldLabel">First Name*</p>
+                                <TextField fullWidth className="formfield" color="secondary" required id="outline-required"/>
+                            </Grid>
+                            <Grid xs={6} paddingInline={2}>
+                                <p className="fieldLabel">Last Name*</p>
+                                <TextField fullWidth className="formfield" color="secondary"/>
+                            </Grid>
+                            <Grid xs={12} paddingInline={2}>
+                            <p className="fieldLabel">Email*</p>
+                            <TextField fullWidth className="formfield" color="secondary"/>
+                            </Grid>
+                            <Grid xs={12} paddingInline={2}>
+                                <p className="fieldLabel">Date of Birth*</p>
+                                <TextField fullWidth className="formfield" color="secondary"/>
+                            </Grid>
+                            <Grid xs={12} paddingInline={2}>
+                                <p className="fieldLabel">Password*</p>
+                                <TextField fullWidth className="formfield" color="secondary"/>
+                            </Grid>
+                            <Grid xs={12} paddingInline={2}>
+                                <p className="fieldLabel">Confirm Password*</p>
+                                <TextField fullWidth className="formfield" color="secondary"/>
+                                <p className="fieldLabel">Use 8 or more characters with a mix of lower and upper case letters & numbers.</p>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+        </Container>
     )
 }
 
