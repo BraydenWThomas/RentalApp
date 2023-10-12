79756b890c9439243e095a8b90aa91c7de1a7812
brayden.thomas <brayden.thomas@fdmgroup.com>
2023-10-12 16:23:32 +1100

    allow signup/login with no security

--- src/main/java/com/fdm/FlatBooking/Controller/UserController.java
+++ src/main/java/com/fdm/FlatBooking/Controller/UserController.java
@@ -3,10 +3,14 @@ package com.fdm.FlatBooking.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fdm.FlatBooking.Model.User;
@@ -42,4 +46,24 @@ public class UserController {
                 .orElseThrow();
     }
 
+    // Create User #TODO this will need to be replaced when security added
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    // Verify login #TODO this will need to be replaced when security added
+    @GetMapping("signin")
+    public boolean verifyUser(@PathVariable String email, String password) {
+        // Verify if email false
+        if (userRepository.findById(email).get().getCredentials().getEmail() != email) {
+            return false;
+        }
+        // Verify if password false
+        if (userRepository.findById(email).get().getCredentials().getPassword() != password) {
+            return false;
+        }
+        return true;
+    }
 }
