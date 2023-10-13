e85b138f84e4261963e863664ed4277fd46aa2dc
Alyssa Chin <alyssa.chin@fdmgroup.com>
2023-10-13 13:59:02 +1100

    openLogin variable name change

--- react-frontend/src/Components/HomePage.jsx
+++ react-frontend/src/Components/HomePage.jsx
@@ -5,11 +5,7 @@ import { useEffect, useState } from "react";
 
 const HomePage = () => {
 
-    const [open, setOpen] = useState(false);
-
-    useEffect(() => {
-        console.log(open)
-    }, [])
+    const [openLogin, setOpenLogin] = useState(false);
 
     const buttonStyle = {
         backgroundColor: '#A59DB7',
@@ -17,8 +13,8 @@ const HomePage = () => {
         marginRight: '10px'
     }
 
-    const handleClick = () => {
-        setOpen(true)
+    const handleLoginClick = () => {
+        setOpenLogin(true)
     }
 
     
@@ -27,12 +23,12 @@ const HomePage = () => {
             <div className="top">
                 <p className="slogan">Your Space, Your Wallet, All in One.</p>
                 <div className="login-buttons">
-                    <Button variant="contained" style={buttonStyle} onClick={handleClick}>Login</Button>
+                    <Button variant="contained" style={buttonStyle} onClick={handleLoginClick}>Login</Button>
                     <Button variant="contained" style={buttonStyle}>Sign Up</Button>
                 </div>
             </div>
 
-            <LoginOverlay open={[open, setOpen]} />
+            <LoginOverlay open={[openLogin, setOpenLogin]} />
         </div>
     )
 }
