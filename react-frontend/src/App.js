7f2f1abd6728edaecfb047a784af5608885432a7
Alyssa Chin <alyssa.chin@fdmgroup.com>
2023-10-16 15:21:49 +1100

    remove sign up page (changed to modal)

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Components/HomePage";
-import SignupPage from "./Components/SignupPage";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
@@ -13,7 +12,6 @@ function App() {
 				<Navbar></Navbar>
 				<Routes>
 					<Route path="/" element={<HomePage />}></Route>
-					<Route path="/signup" element={<SignupPage />} />
 				</Routes>
 			</LocalizationProvider>
 		</div>
