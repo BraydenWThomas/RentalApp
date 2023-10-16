cbc0dc0d189dd93330c9be33c397903082abc62a
Jack A <82874057+Jackile13@users.noreply.github.com>
2023-10-16 11:37:14 +1100

    home page UI complete
    The UI contains dummy data for now

--- react-frontend/src/Components/SearchBar.jsx
+++ react-frontend/src/Components/SearchBar.jsx
@@ -1,26 +1,58 @@
 import { Grid, Button, TextField, Stack } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import "../Styles/SearchBar.css";
 
 const SearchBar = () => {
+	const searchBtnStyle = {
+		backgroundColor: "#A59DB7",
+		padding: "1rem",
+	};
 
+	const filterBtnStyle = {
+		backgroundColor: "#A59DB740",
+		color: "black",
+	};
 
-    return (
-        <Grid container>
-            <Grid xs={8}>
-                <div className="searchBox">
-                    <Stack direction="horizonal">
-                        <SearchIcon />
-                        <TextField variant="standard" fullWidth />
-                        <Button variant="standard" className="filterBtn">Filters</Button>
-                    </Stack>
-                </div>
-            </Grid>
-            <Grid xs={4}>
+	const searchIconStyle = {
+		// paddingRight: '10px',
+		color: "gray",
+	};
 
-            </Grid>
-        </Grid>
-    )
-}
+	const searchFieldStyle = {
+		marginBottom: "10px",
+		marginInline: "10px",
+	};
+
+	return (
+		<Grid container className="searchContainer">
+			<Grid xs={10}>
+				<div className="searchBox">
+					<Stack direction="horizonal" className="searchFieldStack">
+						<SearchIcon style={searchIconStyle} />
+						<TextField
+							variant="standard"
+							fullWidth
+							label="Search"
+							style={searchFieldStyle}
+							color="secondary"
+						/>
+						<Button
+							variant="contained"
+							disableElevation
+							style={filterBtnStyle}
+						>
+							Filters
+						</Button>
+					</Stack>
+				</div>
+			</Grid>
+			<Grid xs={2}>
+				<Button style={searchBtnStyle} variant="contained" size="large">
+					Search
+				</Button>
+			</Grid>
+		</Grid>
+	);
+};
 
 export default SearchBar;
