c4acf8f6e405dbc7b5dca53702e520452e7d12b3
Sam <samantha.a.jermyn@gmail.com>
2023-10-17 10:06:36 +1100

    modal added
    modal added

--- react-frontend/src/Components/ProfileWallet.jsx
+++ react-frontend/src/Components/ProfileWallet.jsx
@@ -1,4 +1,9 @@
 import { Button, Grid, TextField, Container, Select, MenuItem, Stack } from "@mui/material";
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import * as React from 'react';
+
 
 import coin from "../Assets/coin.svg";
 import wallet from "../Assets/wallet.svg";
@@ -8,6 +13,27 @@ import "../Styles/Profile.css";
 
 const ProfileWallet = () => {
 
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        backgroundColor: '#E0DFE5',
+      };
+
+      const buttonStyle = {
+        backgroundColor: '#A59DB7',
+        color: 'white',
+        fontFamily: 'Oswald',
+        marginTop: 30,
+        marginBottom:30,
+    }
+
+      const [occurance, setOccurance] = React.useState('');
+      const changeOccurance = (event) => {
+        setOccurance(event.target.value);
+      };
 
 
 
@@ -73,6 +99,10 @@ const ProfileWallet = () => {
        paddingLeft:'20px',
     }
 
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
 	return(
         <Container>
             <Grid container spacing={2}  direction="column" xs={12}>
@@ -83,21 +113,66 @@ const ProfileWallet = () => {
                     <Grid container xs={3}  >
                         <div style={balance}>
                             <p style={balanceDisplay}>
-                                2099.50 $
+                                $ 2,099
                             </p>
                             <p style={normalText} >
                                 Current Balance
                             </p>
+                            
                         </div>
 
                         
                     </Grid>
                     <Grid container  xs={3} >
-                        <div style={walletOptions}>
+                        <div style={walletOptions} onClick={handleOpen}>
                             <img className="coin" src={coin} alt="coin"></img>
                             <p style={normalText}>
                                 Make Payment
                             </p>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box style={style}>
+                                
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                <form>
+                                    <Grid container>
+                                        <Grid xs={12}>
+                                            <h1 style={{textAlign: 'left', paddingInline: '1rem'}} >Payment</h1>
+                                        </Grid>
+                                        <Grid xs={12} paddingInline={2}>
+                                            <p className="fieldLabel">Property ID</p>
+                                            <TextField fullWidth className="formfield" color="secondary" required id="outline-required"/>
+                                        </Grid>
+                                        <Grid xs={12} paddingInline={2}>
+                                            <p className="fieldLabel">Amount</p>
+                                            <TextField fullWidth className="formfield" color="secondary" type="number"/>
+                                        </Grid>
+                                        <Grid xs={12} paddingInline={2}>
+                                            <p className="fieldLabel">How Often*</p>
+                                            <Select className="formfield datePicker" value={occurance} onChange={changeOccurance}>
+                                                <MenuItem value="MONTHLY">Monthly</MenuItem>
+                                                <MenuItem value="FORTNIGHTLY">Fortnightly</MenuItem>
+                                                <MenuItem value="WEEKLY">Weekly</MenuItem>
+                                                <MenuItem value="ONE_OFF">One Off</MenuItem>
+                                            </Select>
+                                        </Grid>
+                                        <Grid xs={12} paddingInline={2}>
+                                            <Button variant="contained"  style={buttonStyle} size="large">
+                                                Pay now
+                                            </Button>
+                                            <Button variant="contained"  style={buttonStyle} size="large">
+                                                Cancel
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                                </Typography>
+                                </Box>
+                            </Modal>
                         </div>
                     </Grid>
                     <Grid container justifyContent="flex-start" xs={3} >
