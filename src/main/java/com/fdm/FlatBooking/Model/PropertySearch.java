279b28280be71ad6aa78811595947628b625d66c
brayden.thomas <brayden.thomas@fdmgroup.com>
2023-10-23 14:10:08 +1100

    modified backend search fields to have a min/max, and changed move in date to dateAvailable

--- src/main/java/com/fdm/FlatBooking/Model/PropertySearch.java
+++ src/main/java/com/fdm/FlatBooking/Model/PropertySearch.java
@@ -4,70 +4,90 @@ import java.util.Date;
 import java.util.List;
 
 public class PropertySearch {
-	private int budget;
-	private int numberOfBedrooms;
-	private int numberOfBathrooms;
-	private int numberOfCarspaces;
+	private int minPrice;
+	private int maxPrice;
+	private int minBedrooms;
+	private int maxBedrooms;
+	private int minBathrooms;
+	private int maxBathrooms;
+	private int minCarSpaces;
+	private int maxCarSpaces;
 	private List<String> preferredFeatures;
-	private Date moveInDate;
-
-	public PropertySearch(int budget, int numberOfBedrooms, int numberOfBathrooms, int numberOfCarspaces,
-			List<String> preferredFeatures, Date moveInDate) {
+	private Date dateAvailable;
+	
+	public PropertySearch(int minPrice, int maxPrice, int minBedrooms, int maxBedrooms, int minBathrooms,
+			int maxBathrooms, int minCarSpaces, int maxCarSpaces, List<String> preferredFeatures, Date dateAvailable) {
 		super();
-		this.budget = budget;
-		this.numberOfBedrooms = numberOfBedrooms;
-		this.numberOfBathrooms = numberOfBathrooms;
-		this.numberOfCarspaces = numberOfCarspaces;
+		this.minPrice = minPrice;
+		this.maxPrice = maxPrice;
+		this.minBedrooms = minBedrooms;
+		this.maxBedrooms = maxBedrooms;
+		this.minBathrooms = minBathrooms;
+		this.maxBathrooms = maxBathrooms;
+		this.minCarSpaces = minCarSpaces;
+		this.maxCarSpaces = maxCarSpaces;
 		this.preferredFeatures = preferredFeatures;
-		this.moveInDate = moveInDate;
+		this.dateAvailable = dateAvailable;
 	}
-
-	public int getBudget() {
-		return budget;
+	public int getMinPrice() {
+		return minPrice;
 	}
-
-	public void setBudget(int budget) {
-		this.budget = budget;
+	public void setMinPrice(int minPrice) {
+		this.minPrice = minPrice;
 	}
-
-	public int getNumberOfBedrooms() {
-		return numberOfBedrooms;
+	public int getMaxPrice() {
+		return maxPrice;
 	}
-
-	public void setNumberOfBedrooms(int numberOfBedrooms) {
-		this.numberOfBedrooms = numberOfBedrooms;
+	public void setMaxPrice(int maxPrice) {
+		this.maxPrice = maxPrice;
 	}
-
-	public int getNumberOfBathrooms() {
-		return numberOfBathrooms;
+	public int getMinBedrooms() {
+		return minBedrooms;
 	}
-
-	public void setNumberOfBathrooms(int numberOfBathrooms) {
-		this.numberOfBathrooms = numberOfBathrooms;
+	public void setMinBedrooms(int minBedrooms) {
+		this.minBedrooms = minBedrooms;
 	}
-
-	public int getNumberOfCarspaces() {
-		return numberOfCarspaces;
+	public int getMaxBedrooms() {
+		return maxBedrooms;
 	}
-
-	public void setNumberOfCarspaces(int numberOfCarspaces) {
-		this.numberOfCarspaces = numberOfCarspaces;
+	public void setMaxBedrooms(int maxBedrooms) {
+		this.maxBedrooms = maxBedrooms;
 	}
-
+	public int getMinBathrooms() {
+		return minBathrooms;
+	}
+	public void setMinBathrooms(int minBathrooms) {
+		this.minBathrooms = minBathrooms;
+	}
+	public int getMaxBathrooms() {
+		return maxBathrooms;
+	}
+	public void setMaxBathrooms(int maxBathrooms) {
+		this.maxBathrooms = maxBathrooms;
+	}
+	public int getMinCarSpaces() {
+		return minCarSpaces;
+	}
+	public void setMinCarSpaces(int minCarSpaces) {
+		this.minCarSpaces = minCarSpaces;
+	}
+	public int getMaxCarSpaces() {
+		return maxCarSpaces;
+	}
+	public void setMaxCarSpaces(int maxCarSpaces) {
+		this.maxCarSpaces = maxCarSpaces;
+	}
 	public List<String> getPreferredFeatures() {
 		return preferredFeatures;
 	}
-
 	public void setPreferredFeatures(List<String> preferredFeatures) {
 		this.preferredFeatures = preferredFeatures;
 	}
-
-	public Date getMoveInDate() {
-		return moveInDate;
+	public Date getDateAvailable() {
+		return dateAvailable;
 	}
-
-	public void setMoveInDate(Date moveInDate) {
-		this.moveInDate = moveInDate;
+	public void setDateAvailable(Date dateAvailable) {
+		this.dateAvailable = dateAvailable;
 	}
 
 }
