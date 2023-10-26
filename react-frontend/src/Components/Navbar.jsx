3b3d1e389c64ea5fc80a7d7ce1b1bb8e6cf409f5
Jack A <82874057+Jackile13@users.noreply.github.com>
2023-10-27 09:29:36 +1100

    clicking logo goes to homepage

--- react-frontend/src/Components/Navbar.jsx
+++ react-frontend/src/Components/Navbar.jsx
@@ -3,20 +3,29 @@ import { Link, useNavigate } from "react-router-dom";
 import logo from "../Assets/logo.png";
 
 const Navbar = (props) => {
-	const user = props.user
-	const setUser = props.setUser
+	const user = props.user;
+	const setUser = props.setUser;
+	const nav = useNavigate();
 	return (
-
 		<nav className="navbar">
 			<div>
-				<img className="navbar-logo" src={logo} alt="RENTEX"></img>
+				<img
+					className="navbar-logo"
+					src={logo}
+					alt="RENTEX"
+					onClick={() => {
+						nav("/");
+					}}
+				></img>
 			</div>
 			{props.isLoggedIn && (
 				<div className="navbar-links">
 					<Link to="/">Home</Link>
 					<Link to="/my-wallet">My Wallet</Link>
 					<Link to="/SavedProperties">Saved Properties</Link>
-					<Link to="/profile" state={{ from: user }} >My Profile</Link>
+					<Link to="/profile" state={{ from: user }}>
+						My Profile
+					</Link>
 				</div>
 			)}
 		</nav>
