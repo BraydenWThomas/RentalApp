a4604d281dc95f76c32e9b21c859d51d8dd19ce9
Jack A <82874057+Jackile13@users.noreply.github.com>
2023-10-27 09:33:58 +1100

    fixed java error

--- src/main/java/com/fdm/FlatBooking/FlatBookingApplication.java
+++ src/main/java/com/fdm/FlatBooking/FlatBookingApplication.java
@@ -107,7 +107,7 @@ public class FlatBookingApplication implements CommandLineRunner {
 		for (Property property : properties) {
 			System.out.println(property.getAddress().getStreet());
 		}
-		
+
 		ArrayList propType = new ArrayList();
 		propType.add("House");
 		propType.add("Apartment");
@@ -115,7 +115,7 @@ public class FlatBookingApplication implements CommandLineRunner {
 		propType.add("Grannyflat");
 		propType.add("Room");
 		propType.add("Unit");
-		
+
 		properties = propertyRepository.getPropertyWithFilters(0, 3, 0, 3, 0, 501, 0, 3, 0, 30000, "Apartment",
 				true, propType);
 
@@ -223,29 +223,29 @@ public class FlatBookingApplication implements CommandLineRunner {
 
 		ArrayList<Property> properties = new ArrayList<>();
 		properties.add(new Property("House", 500, 2000, users.get(0).getId(), null, addrs.get(0), "1 Red Hotel",
-				pds.get(0), features, new Date(20000000), true));
+				pds.get(0), facilities, new Date(20000000), true));
 		properties.add(new Property("Apartment", 500, 2000, users.get(0).getId(), null, addrs.get(1), "1 Green House",
-				pds.get(1), features, new Date(20000000), true));
+				pds.get(1), facilities, new Date(20000000), true));
 		properties.add(new Property("Townhouse", 500, 2000, users.get(1).getId(), null, addrs.get(2), "2 Green Houses",
-				pds.get(2), features, new Date(20000000), true));
+				pds.get(2), facilities, new Date(20000000), true));
 		properties.add(new Property("Grannyflat", 500, 2000, users.get(2).getId(), null, addrs.get(3), "3 Green Houses",
-				pds.get(3), features, new Date(20000000), true));
+				pds.get(3), facilities, new Date(20000000), true));
 		properties.add(new Property("Room", 500, 2000, users.get(3).getId(), null, addrs.get(4), "1 Red Hotel",
-				pds.get(4), features, new Date(20000000), true));
+				pds.get(4), facilities, new Date(20000000), true));
 		properties.add(new Property("Unit", 500, 2000, users.get(4).getId(), null, addrs.get(5), "4 Green Houses",
-				pds.get(5), features, new Date(20000000), true));
+				pds.get(5), facilities, new Date(20000000), true));
 		properties.add(new Property("House", 500, 2000, users.get(4).getId(), null, addrs.get(6), "3 Green Houses",
-				pds.get(6), features, new Date(20000000), true));
+				pds.get(6), facilities, new Date(20000000), true));
 		properties.add(new Property("Apartment", 500, 2000, users.get(6).getId(), null, addrs.get(7), "1 Red Hotel",
-				pds.get(7), features, new Date(20000000), true));
+				pds.get(7), facilities, new Date(20000000), true));
 		properties.add(new Property("Townhouse", 500, 2000, users.get(5).getId(), null, addrs.get(8), "1 Red Hotel",
-				pds.get(8), features, new Date(20000000), true));
+				pds.get(8), facilities, new Date(20000000), true));
 		properties.add(new Property("Grannyflat", 500, 2000, users.get(6).getId(), null, addrs.get(9), "1 Green House",
-				pds.get(9), features, new Date(20000000), true));
+				pds.get(9), facilities, new Date(20000000), true));
 		properties.add(new Property("Room", 500, 2000, users.get(6).getId(), null, addrs.get(10), "2 Green Houses",
-				pds.get(10), features, new Date(20000000), true));
+				pds.get(10), facilities, new Date(20000000), true));
 		properties.add(new Property("Unit", 500, 2000, users.get(3).getId(), null, addrs.get(11), "4 Green Houses",
-				pds.get(11), features, new Date(20000000), true));
+				pds.get(11), facilities, new Date(20000000), true));
 
 		propertyRepository.saveAll(properties);
 
@@ -270,7 +270,7 @@ public class FlatBookingApplication implements CommandLineRunner {
 
 	private void createTransactions() {
 		List<User> users = userRepository.findAll();
-		System.out.println(users); 
+		System.out.println(users);
 		ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 		transactions.add(
 				new Transaction(users.get(0).getId(), users.get(1).getId(), new Date(20000000), 1000, "ref1"));
